78673fc46bd0f7923b0aeb781f9e9c9238449790
MorozovAP <andre15@inbox.ru>
2019-01-10 08:22:43 +0400

    +sphere

---
+++ Sphere.h
@@ -0,0 +1,33 @@
+﻿#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+#pragma once
+#endif
+
+#ifndef _SPHERE_H_
+#define _SPHERE_H_
+
+struct Sphere
+{
+public:
+	double x;
+	double y;
+	double z;
+	double radius;
+	double r;
+	double g;
+	double b;
+	double a;
+
+	Sphere(double x, double y, double z, double radius, double r, double g, double b, double a)
+	{
+		this->x = x;
+		this->y = y;
+		this->z = z;
+		this->radius = radius;
+		this->r = r;
+		this->g = g;
+		this->b = b;
+		this->a = a;
+	}		
+};
+
+#endif
