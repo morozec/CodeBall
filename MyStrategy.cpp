d7c4f76410b6917e804fcbf79ba67d1692d9314e
MorozovAP <andre15@inbox.ru>
2019-02-01 14:55:11 +0400

    учет _lastMyCollisionTick при защите ворот

--- MyStrategy.cpp
+++ MyStrategy.cpp
@@ -2007,7 +2007,7 @@ model::Action MyStrategy::SetAttackerAction(const model::Robot & me,
 	const auto startPos = Helper::GetRobotPosition(me);
 	const auto startVel = Helper::GetRobotVelocity(me);
 
-	bool needUseDefenceNitro = position == -1 && me.nitro_amount > 0
+	bool needUseDefenceNitro = position == -1 && me.nitro_amount > 0 && (_lastMyCollisionTick == -1 || _isMeGoalPossible)
 		&& (_defenderMovePoints.count(me.id) == 0 || !std::get<3>(_defenderMovePoints[me.id]).isGoalScored);
 	const auto goalSearchTicks = 40;
 	if (needUseDefenceNitro)
