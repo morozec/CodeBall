ad65f0bb92547648f5d9f59f82ed9f3de76dcd56
morozec <andre15@inbox.ru>
2019-02-02 21:56:19 +0400

    BEC added

---
+++ BallEntityContainer.cpp
@@ -0,0 +1,18 @@
+﻿#include "BallEntityContainer.h"
+
+BallEntityContainer::BallEntityContainer(
+	BallEntity ballEntity, double collisionTime, bool isGoalScored, double goalTime, int collisionsCount, BallEntity collideBallEntity, int nitroDest)
+{
+	this->ResBallEntity = ballEntity;
+	this->collisionTime = collisionTime;
+	this->isGoalScored = isGoalScored;
+	this->goalTime = goalTime;
+	this->collisionsCount = collisionsCount;
+	this->CollideBallEntity = collideBallEntity;
+	this->NitroDest = nitroDest;
+}
+
+double BallEntityContainer::GetFullGoalTime() const
+{
+	return collisionTime + goalTime;
+}
---
+++ BallEntityContainer.h
@@ -0,0 +1,35 @@
+﻿#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+#pragma once
+#endif
+#include "BallEntity.h"
+
+#ifndef _BallEntityContainer_H_
+#define _BallEntityContainer_H_
+
+class BallEntityContainer
+{
+public:
+	BallEntity ResBallEntity;
+	double collisionTime;//время на разгон и прыжок
+	bool isGoalScored;
+	double goalTime;//время после прыжка до гола
+	int collisionsCount;
+	BallEntity CollideBallEntity;//мяч после коллизии с ареной
+
+	int NitroDest; //-1 - на работа врага, 0 - без нитры, 1 - чтобы забить гол
+
+	BallEntityContainer()
+	{
+		ResBallEntity = BallEntity(); collisionTime = -1; 
+		isGoalScored = false; goalTime = -1; collisionsCount = 0;
+		CollideBallEntity = BallEntity(); NitroDest = 0;
+	}
+	BallEntityContainer(
+		BallEntity ballEntity, double collisionTime, bool isGoalScored, double goalTime,
+		int collisionsCount, BallEntity collideBallEntity, int nitroDest);
+
+	double GetFullGoalTime() const;
+};
+
+#endif
+
