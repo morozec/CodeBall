4706bbe54529de7f6fc22cd88988c7b45b7c0722
MorozovAP <andre15@inbox.ru>
2019-02-12 11:51:39 +0400

    еще файлы

---
+++ StopContainer.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "Vector3D.h"
+
+struct StopContainer
+{
+	Vector3D robotPosition;
+	Vector3D robotVelocity;
+
+	int stopMicroTicks;
+	Vector3D stopPos;
+	Vector3D stopA;
+
+
+	StopContainer(const Vector3D& robot_position, const Vector3D& robot_velocity, int stop_micro_ticks,
+		const Vector3D& stop_pos, const Vector3D& stop_a)
+		: robotPosition(robot_position),
+		  robotVelocity(robot_velocity),
+		  stopMicroTicks(stop_micro_ticks),
+		  stopPos(stop_pos),
+		  stopA(stop_a)
+	{
+	}
+};
